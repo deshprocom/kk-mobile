10541f74937ea879f01012d1d25e6c4d5dcbdd07
lorne <2270333671@qq.com>
2018-09-26 17:25:20 +0800

    新增图片 fastBtn的样式

--- src/components/indexPage/HomeCardNav.js
+++ src/components/indexPage/HomeCardNav.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import { Card, Grid, Flex } from 'antd-mobile';
+import React, {Component} from 'react';
+import {Card, Grid, Flex} from 'antd-mobile';
 import styles from './index.less';
+import {Images} from '../../Thems';
 
 const actions = Array.from(new Array(8)).map((_val, i) => ({
   icon: 'https://gw.alipayobjects.com/zos/rmsportal/nywPmnTAvTmLusPxHPSu.png',
@@ -9,20 +10,24 @@ const actions = Array.from(new Array(8)).map((_val, i) => ({
 
 const services = [
   {
-    icon: 'https://gw.alipayobjects.com/zos/rmsportal/nywPmnTAvTmLusPxHPSu.png',
+    icon: Images.navigation2.weather,
     text: '天气',
+    size: {height: 23, width: 23},
   },
   {
-    icon: 'https://gw.alipayobjects.com/zos/rmsportal/nywPmnTAvTmLusPxHPSu.png',
+    icon: Images.navigation2.fast_food,
     text: '快餐',
+    size: {height: 22, width: 22},
   },
   {
-    icon: 'https://gw.alipayobjects.com/zos/rmsportal/nywPmnTAvTmLusPxHPSu.png',
+    icon: Images.navigation2.round_trip,
     text: '人闻',
+    size: {height: 20, width: 20},
   },
   {
-    icon: 'https://gw.alipayobjects.com/zos/rmsportal/nywPmnTAvTmLusPxHPSu.png',
+    icon: Images.navigation2.convenient,
     text: '便民',
+    size: {height: 18, width: 18}
   },
 ];
 
@@ -30,19 +35,19 @@ export default class HomeCardNav extends Component {
   render() {
     return (
       <Card full className={styles.customCard}>
-        <Grid data={actions} hasLine={false} />
-        <Card.Body >
+        <Grid data={actions} hasLine={false}/>
+        <Card.Body>
           {/*<Grid data={services}*/}
-                {/*hasLine={false}*/}
-                {/*className={styles.customCard}*/}
-                {/*renderItem={dataItem => (*/}
-               
-                {/*)}*/}
+          {/*hasLine={false}*/}
+          {/*className={styles.customCard}*/}
+          {/*renderItem={dataItem => (*/}
+
+          {/*)}*/}
           {/*/>*/}
           <Flex className={styles.customFlex}>
             {services.map(dataItem => (
-              <Flex.Item key={dataItem.text}>
-                <img src={dataItem.icon} alt="" className={styles.customImg} />
+              <Flex.Item key={dataItem.text} className={styles.customView}>
+                <img style={dataItem.size} src={dataItem.icon} alt="" className={styles.customImg}/>
                 <span>{dataItem.text}</span>
               </Flex.Item>
             ))}
