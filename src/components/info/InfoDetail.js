c8b270c0c905e1d9dd8a64b07e603f7e5ccb0707
lorne <2270333671@qq.com>
2018-09-27 15:00:54 +0800

    资讯详情样式调节

--- src/components/info/InfoDetail.js
+++ src/components/info/InfoDetail.js
@@ -4,11 +4,12 @@ import { Card } from 'antd-mobile';
 export default class InfoDetail extends Component {
   render() {
     const { infoDetail } = this.props;
-  
+
     return (
       <Card>
         <Card.Header
           title={infoDetail.title}
+          style={{display:'flex',flexDirection:'column',alignItems:'center'}}
         />
         <Card.Body>
           <div className="renderHtmlData"
